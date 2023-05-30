753be88a5e4e914715dd9796944c94640774237c
MrCheat <40684650+MrCheatEugene@users.noreply.github.com>
2023-05-30 10:34:09 +0300

    Update CraftMapCanvas.java

--- proxy/src/main/java/ru/leymooo/botfilter/captcha/generator/map/CraftMapCanvas.java
+++ proxy/src/main/java/ru/leymooo/botfilter/captcha/generator/map/CraftMapCanvas.java
@@ -3,7 +3,7 @@ package ru.leymooo.botfilter.captcha.generator.map;
 import java.awt.image.BufferedImage;
 import java.util.Arrays; 
 import javax.imageio.ImageIO;
-import java.xml.bind.DatatypeConverter;
+import javax.*;
 import javax.xml.bind.DatatypeConverter;
 import ru.leymooo.botfilter.packets.MapDataPacket;
 import java.io.ByteArrayInputStream;
